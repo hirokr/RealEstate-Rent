981ebc2e3dc30d9c266702343bdab1ded5ebd645
hirokr <hrr3645@gmail.com>
2025-09-08 02:39:46 +0600

    fix: correct user retrieval logic in GetUser decorator

--- server/src/auth/decorator/get-user.decorator.ts
+++ server/src/auth/decorator/get-user.decorator.ts
@@ -4,8 +4,8 @@ export const GetUser = createParamDecorator(
   (data: unknown | undefined, ctx: ExecutionContext) => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
     if (data) {
-      return request.user?.[data as keyof Express.Request['user']];
+      return request
     }
-    return request.user;
+    return request;
   },
 );
